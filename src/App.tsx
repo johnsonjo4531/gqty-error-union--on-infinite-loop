ed482a19c80ddbf68ab8c39a5bf3aa7caf94309b
John Johnson II <johnsonjo4531@gmail.com>
2021-10-20 16:16:58 -0600

    added fix to typeerror and comment

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,12 @@ function App() {
   const node = query.node({ id: btoa(`people:${id.toString()}`) });
   return (
     <div className="App">
-      {atob(node?.id)}
-      {/* Uncomment this line to see the problem */}
+      {atob(node?.id ?? "")}
+      {/* Uncomment this line to see the problem (no user interaction is required [view your network tab to see the requests come in rapidly.]) */}
       {/* {node?.$on.Person?.name} */}
-      <button>Prev</button><button onClick={() => {
+      <button onClick={() => {
+        setId(id - 1)
+      }}>Prev</button><button onClick={() => {
         setId(id+1)
       }}>Next</button>
     </div>
